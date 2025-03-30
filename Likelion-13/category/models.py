f94cba67ab7ce31b4adc1c68f9e41a1578c5b8ef
jjjaehong <woghd3014@gmail.com>
2025-03-30 20:42:40 +0900

    유저와 카테고리의 다대다 관계를 일대다, 다대일 관계로 풀기

--- Likelion-13/category/models.py
+++ Likelion-13/category/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)  # 카테고리 이름
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='categories')
     def __str__(self):
         return self.name
