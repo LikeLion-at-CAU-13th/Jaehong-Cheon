2e4ca414bb091c7afbf4301112ecb8f62fd86360
jjjaehong <woghd3014@gmail.com>
2025-03-31 16:53:20 +0900

    Post에서 created와 updated 삭제 !

--- Likelion-13/posts/models.py
+++ Likelion-13/posts/models.py
@@ -21,8 +21,7 @@ class Post(BaseModels):
     title = models.CharField(max_length=30)
     content = models.TextField()
     status = models.CharField(max_length=15, choices=CHOICES, default='STORED')
-    created = models.DateTimeField(auto_now_add=True) 
-    updated = models.DateTimeField(auto_now=True)  
+ 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
     
 
